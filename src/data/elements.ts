013f351177b6cea099b4a95330550bd3f0e113cb
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2025-05-18 19:16:07 +0300

    added tweaks

--- src/data/elements.ts
+++ src/data/elements.ts
@@ -1,6 +1,6 @@
 import { ElementParam } from "../types/elementParam";
 
 export const elements: ElementParam[] = [
-  { title: "Параметры окон", label: "Добавить окно" },
+  { title: "Параmетры окон", label: "Добавить окно" },
   { title: "Параметры дверей", label: "Добавить дверь" },
 ];
